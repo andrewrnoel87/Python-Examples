ea8f90ef90b533ce65a133d8f1477c9712319d0c
andrewrnoel87 <andrewrnoel87@gmail.com>
2023-09-06 17:02:51 -0400

    adding a py program that does simple I/O

---
+++ programmingexpert-study/console_input_example.py
@@ -0,0 +1,7 @@
+input("Press enter to begin!")
+
+name = input("Enter name: ")
+age = input("Hello, " + name + " what is your age: ")
+
+print("Hello,", name, "you are", age, "years old!")
+print("yup")
