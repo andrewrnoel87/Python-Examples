30236787d474970bf0e469a06ed14636202d5d20
andrewrnoel87 <andrewrnoel87@gmail.com>
2023-10-04 18:03:39 -0400

    puts together coded strings now

--- programmingexpert-study/CaesarCipher.py
+++ programmingexpert-study/CaesarCipher.py
@@ -1,12 +1,24 @@
 #def caesar_cipher(string, offset):
-offset = 2
+string = "apple"
+string_list = [i for i in string]
+offset = 5
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 shifted_alphabet = [i for i in alphabet]
 
 for i in range(len(shifted_alphabet)):
     shifted_alphabet[i] = alphabet[i - offset]
+
+for i in range(len(string_list)):
+    char = string_list[i]
+    target_char = shifted_alphabet[alphabet.index(char)]
+    string_list[i] = target_char
+
+coded_string = ""
+
+for elem in string_list:
+    coded_string += elem
     
-print(shifted_alphabet)
+print(coded_string)
 
 
 
