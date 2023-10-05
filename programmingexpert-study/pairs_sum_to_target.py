2cea1803bc409e294e0b34dbb1d1a0e018503f92
andrewrnoel87 <andrewrnoel87@gmail.com>
2023-10-05 15:34:21 -0400

    Create pairs_sum_to_target.py

---
+++ programmingexpert-study/pairs_sum_to_target.py
@@ -0,0 +1,17 @@
+list1 = [1, -2, 4, 5, 9]
+
+list2 = [4, 2, -4, -4, 0]
+
+target = 5
+
+def pairs_sum_to_target(list1, list2, target):
+    # Write your code here.
+
+
+
+
+
+    
+    return list1 + list2
+
+print(pairs_sum_to_target(list1, list2, target))
