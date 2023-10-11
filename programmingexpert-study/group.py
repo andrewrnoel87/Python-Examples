5c22df39e27eccd8a0d0c57e015a25aab2536d90
andrewrnoel87 <andrewrnoel87@gmail.com>
2023-10-11 15:17:51 -0400

    Create group.py

---
+++ programmingexpert-study/group.py
@@ -0,0 +1,33 @@
+class Group:
+    def __init__(self, name, members=[]):
+        self.name = name
+        self.members = members
+
+    def add(self, name):
+        self.members.append(name)
+
+    def delete(self, name):
+        if name in self.members:
+            self.members.remove(name)
+        else:
+            raise Exception("Member not in group.")
+
+    def get_members(self):
+        return sorted(self.members)
+
+    def merge(self, group):
+        merged_members = self.members + group.members
+        merged_group = Group(self.name + '&' + group.name, merged_members)
+        return merged_group
+    
+g1 = Group('A-Team', ['Tim', 'Joe'])
+g2 = Group("tunnelbees", ['a', 'b', 'c'])
+g1.add('Sally')
+g1.add('Billy')
+members = g1.get_members()
+print(members)
+g1.delete('Joe')
+members = g1.get_members()
+print(members)
+print(g1.merge(g2).get_members())
+#g1.delete('Joe')
