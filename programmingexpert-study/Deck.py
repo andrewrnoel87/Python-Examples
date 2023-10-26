330025fe918c9cc9e2623910591ad8c39bfdaa0a
andrewrnoel87 <andrewrnoel87@gmail.com>
2023-10-26 15:25:08 -0400

    Update Deck.py

--- programmingexpert-study/Deck.py
+++ programmingexpert-study/Deck.py
@@ -1,2 +1,7 @@
 class Deck:
-    
+    values = [str(i) for i in range(2, 11)] + ["J", "Q", "K", "A"]
+    suits = ["D", "H", "C", "S"]  # Diamond, Heart, Club, Spade
+
+    def __init__(self):
+        deck = []
+        
