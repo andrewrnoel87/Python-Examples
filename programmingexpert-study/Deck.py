982a977d70070e0d69ce25af15dc4e3f93c25e0f
andrewrnoel87 <andrewrnoel87@gmail.com>
2023-10-26 15:39:27 -0400

    Update Deck.py

--- programmingexpert-study/Deck.py
+++ programmingexpert-study/Deck.py
@@ -3,5 +3,14 @@ class Deck:
     suits = ["D", "H", "C", "S"]  # Diamond, Heart, Club, Spade
 
     def __init__(self):
-        deck = []
+        self.deck = []
+        self.fill_deck()
+
+    def fill_deck(self):
+        for value in Deck.values:
+            for suit in Deck.suits:
+                self.deck.append(value + suit)
+
+d = Deck()
+
         
