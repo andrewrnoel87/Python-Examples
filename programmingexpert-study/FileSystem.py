258660f7ea3cf71de8f9cd1cf93596951a37e8c1
andrewrnoel87 <andrewrnoel87@gmail.com>
2023-11-01 13:05:24 -0400

    Create FileSystem.py

---
+++ programmingexpert-study/FileSystem.py
@@ -0,0 +1,90 @@
+class FileSystem:
+    def __init__(self):
+        self.root = Directory("/")
+
+    def create_directory(self, path):
+        # Write your code here.
+        pass
+
+    def create_file(self, path, contents):
+        # Write your code here.
+        pass
+
+    def read_file(self, path):
+        # Write your code here.
+        pass
+
+    def delete_directory_or_file(self, path):
+        # Write your code here.
+        pass
+
+    def size(self):
+        # Write your code here.
+        pass
+
+    def __str__(self):
+        return f"*** FileSystem ***\n" + self.root.__str__() + "\n***"
+    
+    @staticmethod
+    def _validate_path(path):
+        if not path.startswith("/"):
+            raise ValueError("Path should start with `/`.")
+
+
+    def _find_bottom_node(self, node_names):
+        # Write your code here.
+        pass
+
+
+class Node:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"{self.name} ({type(self).__name__})"
+
+
+class Directory(Node):
+    def __init__(self, name):
+        super().__init__(name)
+        self.children = {}
+
+    def add_node(self, node):
+        self.children[node.name] = node
+
+    def delete_node(self, name):
+        del self.children[name]
+
+    def __str__(self):
+        string = super().__str__()
+
+        children_strings = []
+        for child in list(self.children.values()):
+            child_string = child.__str__().rstrip()
+            children_strings.append(child_string)
+
+        children_combined_string = indent("\n".join(children_strings), 2)
+        string += "\n" + children_combined_string.rstrip()
+        return string
+
+
+class File(Node):
+    def __init__(self, name):
+        super().__init__(name)
+        self.contents = ""
+
+    def write_contents(self, contents):
+        self.contents = contents
+
+    def __len__(self):
+        return len(self.contents)
+
+    def __str__(self):
+        return super().__str__() + f" | {len(self)} characters"
+
+
+def indent(string, number_of_spaces):
+    spaces = " " * number_of_spaces
+    lines = string.split("\n")
+    indented_lines = [spaces + line for line in lines]
+    return "\n".join(indented_lines)
